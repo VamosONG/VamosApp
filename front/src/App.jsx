3101cdfb4dd4685d202efa786f2bdb862bb3d428
VamosONG <ong.vamos.2024@gmail.com>
2024-01-09 17:36:53 -0300

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Landing from './views/landing/landingComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Landing/>
+    </>
+  )
+}
+
+export default App
---
+++ front/src/views/landing/landingComponent.jsx
@@ -0,0 +1,11 @@
+function Landing() {
+    
+  
+    return (
+      <>
+        <p>Funciona Landing</p>
+      </>
+    )
+  }
+  
+  export default Landing
