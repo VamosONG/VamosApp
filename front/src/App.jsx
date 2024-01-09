303cebcc7a297afb0171ba83dfb9ad1f5720d30f
Kleibert <kleibertmedina@gmail.com>
2024-01-09 17:18:28 -0500

    addChakraProvider

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,14 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './views/landing/landingComponent'
+import LoginForm from './views/Forms/Login/Login'
+import {Routes, Route} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Landing/>
-
+      <button><Link to='/login' >Login</Link></button>
+      
+      <Routes>
+        <Route path='/login' Component={LoginForm}/>
+      </Routes>
+      
     </>
   )
 }
