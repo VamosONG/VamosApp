1158892afd611732056fb087126461f0c37a6d29
JoseBrav <josebravo2015@gmail.com>
2024-01-09 20:45:19 -0300

    Models

---
+++ Back/src/Models/admin.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+
+
+module.exports = (sequelize) => {
+
+    sequelize.define('Admin', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        TotalTrips: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Drivers: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Users: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+        { timesTamps: false });
+};
