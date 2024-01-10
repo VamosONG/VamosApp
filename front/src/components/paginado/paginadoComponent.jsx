a361f4a18dcc1eb4a5042c9bd9b18187928235c0
rubencorba <rubencorba@gmail.com>
2024-01-10 19:15:59 -0300

    pequeños cambios de prueba

--- front/src/components/paginado/paginadoComponent.jsx
+++ front/src/components/paginado/paginadoComponent.jsx
@@ -47,14 +47,3 @@ function Paginado() {
 
 
 export default Paginado
-
-/* const BtnPage = ({ name, onClick, active }) => (
-    <div className={${style.btnIcon} ${active ? style.activeBtn : ''}}>
-        <button className={style.btn} name={name} onClick={onClick}>
-            {name === 'prev' ? '<' : '>'}
-        </button>
-        <p className={style.btnText}>{name.charAt(0).toUpperCase() + name.slice(1)}</p>
-    </div>
-);
-
-export default BtnPage; */
