daf6facf85343dda635b5efdd055330093ba8715
unknown <rocio.sosa94@gmail.com>
2024-01-10 09:16:11 -0300

    navBar

--- front/src/components/navBar/NavBar.jsx
+++ front/src/components/navBar/NavBar.jsx
@@ -1,22 +1,39 @@
-import {  Button, Flex, Stack, AvatarGroup, Avatar, Box, Spacer,  } from "@chakra-ui/react"
+import {
+  Button,
+  Flex,
+  Stack,
+  AvatarGroup,
+  Avatar,
+  Box,
+  Spacer,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-    return (
-        <Flex as="nav" bg= "blue.200" alignItems="center" justify="space-around" >
-            <Stack direction="row" spacing={15} align="center" >
-            <Button colorScheme='blue'>Inicio</Button>
-            <Button colorScheme='blue'>Reservar</Button>
-            <Button colorScheme='blue'>Preguntas Frecuentes</Button>
-            <Button colorScheme='blue' >Nosotros</Button>
-            </Stack>
-            
-            <Spacer/>
-            <AvatarGroup spacing='1rem'>
-                <Avatar bg='teal.500' />
-            </AvatarGroup>
-            <Box>
-                User
-            </Box>
-        </Flex>
-    )
-}
+  return (
+    <Flex as="nav" bg="blue.200" alignItems="center" justify="space-around">
+        
+      <Stack direction="row" spacing={15} align="center">
+        <Link to="/landing">
+          <Button colorScheme="blue">Inicio</Button>
+        </Link>
+        <Link to="/reservas">
+         <Button colorScheme="blue">Reservas</Button>
+        </Link>
+        <Link to="/frecuentes">
+         <Button colorScheme="blue">Preguntas Frecuentes</Button>
+        </Link>
+        <Link to="/nosotros">
+         <Button colorScheme="blue">Nosotros</Button>
+        </Link>
+      </Stack>
+
+      <Spacer />
+      <AvatarGroup spacing="1rem">
+        <Avatar bg="teal.500" />
+      </AvatarGroup>
+      <Box>User</Box>
+    </Flex>
+  );
+};
 export default NavBar;
