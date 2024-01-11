f7f2b412695b90355c5a27e92258cc920f2779be
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-01-10 21:10:20 -0300

    Completado controller de Zones

---
+++ Back/src/Controllers/ZonesControllers/updateZone.js
@@ -0,0 +1,29 @@
+const {zone}=require('../../Models/zone');
+
+module.exports=async(id,name)=>{
+    if(!id)
+        throw new Error('No se recibió id para encontrar la zona.');
+    if(!name)
+        throw new Error('No se recibieron datos para modificar la zona.');
+
+    const existingZone=await zone.findOne({
+        where:{id: id}
+    })
+    if(!existingZone)
+        throw new Error(`No existe zona con el id ${id} en la base de datos.`);
+    else{
+        await zone.update({
+            fields:{
+                place:name,
+            },
+            where: {
+                id: id
+            }
+        })
+
+        existingZone=await zone.findOne({
+            where:{id: id}
+        })
+        return existingZone;
+    }
+}
