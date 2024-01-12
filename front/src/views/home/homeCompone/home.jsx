2a96d0d9097fbb17cbd27f008b753c7d435638a3
Han10MD <milton21delgado@gmail.com>
2024-01-12 12:09:23 -0300

    Cambios en el Home

--- front/src/views/home/homeCompone/home.jsx
+++ front/src/views/home/homeCompone/home.jsx
@@ -1,45 +1,83 @@
 import React from "react";
-import { Box, Button, Flex, Text, Heading, VStack } from '@chakra-ui/react';
-import theme from "../../../Theme";
-
+import { Box, Button, Flex, Text, Heading, VStack, Stack } from '@chakra-ui/react';
+import theme from "../../../theme/Theme";
+import bgImage from "../../../assets/Aeropuerto.jpg"
+import { color } from "framer-motion";
 
 const Home = () => {
 
     return (
-        <Box 
-        p={4}
-        height="100vh"
-        position="relative"
+        <Box
+        h='100vh'
+        bg='gray'
+        bgImage={`url(${bgImage})`}
+        bgRepeat='no-repeat'
+        bgSize='cover'
         >
-            <Box
-            position="absolute"
-            top={0}
-            left={0}
-            width="100%"
-            height="100%"
-            bg={`url('https://scontent.fcor13-1.fna.fbcdn.net/v/t1.6435-9/52358920_2269546896622104_8051910507050827776_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=MJHbQK6mrwkAX9i4iVu&_nc_ht=scontent.fcor13-1.fna&oh=00_AfAZcN-nU7V_RbcBdFdE33J9jwe2tvhIo3mLn4xWvZLCqw&oe=65C6925F') center/cover fixed no-repeat`}
-            bgColor="rgba(0, 0, 0, 0.5)"
-            filter="brightness(60%) blur(1px)"
-            zIndex={-1}
-            />
-            <Box margin="10rem">
-                <Flex direction="column" align="center">
-                    <VStack spacing={8} align="center">
-                        <Heading as="h2" size="x1" fontFamily="'DIN Alternate Black', sans-serif" color="white" fontSize="6xl" textAlign="left">
-                        Bienvenido a "Vamos", una empresa social comprometida con brindar servicios de taxi aeropuerto.</Heading>
-                        <Text textAlign="left" fontFamily="'DIN Medium'," color="white" fontSize="3xl"
-                        >Nos enorgullece ofrecer traslados desde Tumbes e Talar hacia domicilios y hoteles, con tarifas asequibles a partir de 20 soles (aprox. US$5). 
-                        Nuestra misión va más allá de brindar un servicio de transporte; también creamos 
-                        oportunidades laborales para adultos mayores que desean seguir emprendiendo. 
-                        Con 'Vamos', tienes la garantía de vehículos nuevos y cómodos, disponible las 24 horas.
-                        </Text>
-                        <Button colorScheme="blue" >Reserva</Button>
-                    </VStack>
-                </Flex>
-            </Box>
+            <Flex
+            direction='column'
+            alignItems='center'
+            justify='center'
+            h='100%'
+            bg='rgb(0 0 0 / 40%)'
+            p={['0 10%', null, '0 20%']}
+            >
+            <Heading
+            color='white'
+            textTransform='uppercase'
+            textAlign='left'
+            fontFamily="'DIN Alternate Black', sans-serif"
+            letterSpacing='2px'
+            fontSize="6xl"
+            >
+            Bienvenido a "Vamos", una empresa social comprometida con brindar servicios de taxi aeropuerto.
+            </Heading>
+            <Text
+            textAlign="left"
+            fontFamily="'DIN Medium',"
+            color="white"
+            fontSize="3xl"
+            >
+            Nos enorgullece ofrecer traslados desde Tumbes e Talar hacia domicilios y hoteles, con tarifas asequibles a partir de 20 soles (aprox. US$5). 
+            Nuestra misión va más allá de brindar un servicio de transporte; también creamos 
+            oportunidades laborales para adultos mayores que desean seguir emprendiendo. 
+            Con 'Vamos', tienes la garantía de vehículos nuevos y cómodos, disponible las 24 horas.
+            </Text>
+            <Stack
+            direction={{ base: 'column', md: 'row'}}
+            spacing='40px'
+            mt='30px'
+            w={['100%', null, 'auto']}
+            >
+            <Button
+            variant='outline'
+            size='lg'
+            textTransform='uppercase'
+            fontWeight='light'
+            borderRadius='0'
+            color='white'
+            letterSpacing='1px'
+            _hover={{
+                color:'Black',
+                bg: 'white'
+            }}
+            >RESERVA</Button>
+            <Button
+            variant='outline'
+            size='lg'
+            textTransform='uppercase'
+            fontWeight='light'
+            borderRadius='0'
+            color='white'
+            letterSpacing='1px'
+            >+ INFO</Button>
+
+            </Stack>
+            </Flex>
+        
         </Box>
     );
-    
-    }
-    
-    export default Home;
+
+}
+
+export default Home;
