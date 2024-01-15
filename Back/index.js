2a4cf0c74975fb4a05ab8206faeee9401dd1dc87
MONTAR0N <vavilesb1@gmail.com>
2024-01-15 16:11:27 -0300

    forceTrue

--- Back/index.js
+++ Back/index.js
@@ -4,9 +4,8 @@ const { conn } = require('./src/dataBase');
 
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 })
 }).catch(error => console.error(error))
-//probando
