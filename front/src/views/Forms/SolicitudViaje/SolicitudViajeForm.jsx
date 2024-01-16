e152fe11e32874b4524e2904b7867819437bd43f
JoseBrav <josebravo2015@gmail.com>
2024-01-16 14:26:43 -0300

    arreglos

--- front/src/views/Forms/SolicitudViaje/SolicitudViajeForm.jsx
+++ front/src/views/Forms/SolicitudViaje/SolicitudViajeForm.jsx
@@ -11,7 +11,7 @@ import {
     FormLabel,
     Input, Select, Button, Heading, Stack
 } from '@chakra-ui/react'
-import Swal from 'sweetalert2' 
+import Swal from 'sweetalert2'
 
 import { renderToString } from 'react-dom/server';
 
@@ -20,250 +20,192 @@ import { renderToString } from 'react-dom/server';
 
 function SolicitudViajeForm() {
 
-    const dispatch= useDispatch();
-     
-    const infoConfirmacionViaje= useSelector((state)=>state.infoConfirmacionViaje)
+    const dispatch = useDispatch();
+
+    const infoConfirmacionViaje = useSelector((state) => state.infoConfirmacionViaje)
     console.log(infoConfirmacionViaje)
 
 
-    
-    const [input,setInput]=useState({
-        origin:"",
-        destination:"",
-        date:"",
-        hour:"",
-        quantityPassengers:"",
-      });
 
-      const [preferenceId, setPreferenceId] = useState(null);
+    const [input, setInput] = useState({
+        origin: "",
+        destination: "",
+        date: "",
+        hour: "",
+        quantityPassengers: "",
+    });
+
+    const [preferenceId, setPreferenceId] = useState(null);
 
     const confirmationText = (
         <div>
-          <p>Origen: {infoConfirmacionViaje.origin}</p>
-          <p>Destino: {infoConfirmacionViaje.destination}</p>
-          <p>Cantidad de pasajeros: {infoConfirmacionViaje.quantityPassengers}</p>
-          <p>Precio final: {infoConfirmacionViaje.price}</p>
+            <p>Origen: {infoConfirmacionViaje.origin}</p>
+            <p>Destino: {infoConfirmacionViaje.destination}</p>
+            <p>Cantidad de pasajeros: {infoConfirmacionViaje.quantityPassengers}</p>
+            <p>Precio final: {infoConfirmacionViaje.price}</p>
         </div>
     );
 
     useEffect(() => {
-        initMercadoPago('TEST-35665577-40d5-4aa6-8db1-3f478f995b3b', {
+        initMercadoPago('TEST-42b04001-0641-4889-8b14-97f17f509594', {
             locale: "es-PE"
         });
     }, []);
 
     const createPreference = async () => {
         try {
-            const response = await axios.post("http://localhost:3001/merpago/create", {
-                origin: input.origin,
-                destination: input.destination,
-                price: 100, // Cambia esto según el precio real
-                quantityPassengers: input.quantityPassengers,
-            });
-
-            const { id } = response.data;
-            return id;
+          const response = await axios.post("http://localhost:3001/merpago/create", {
+            origin: input.origin,
+            destination: input.destination,
+            price: 100, // Cambia esto según el precio real
+            quantityPassengers: input.quantityPassengers,
+          });
+    
+          const { id } = response.data;
+          return id;
         } catch (error) {
-            console.log(error);
-        }
-    };
+          console.log(error);
+        }}
 
-    const handlePayment = async () => {
-        const id = await createPreference();
-        if (id) {
-            setPreferenceId(id);
-
-            await Swal.fire({
-                title: "Procesando pago...",
-                html: "<p>Redirigiendo a Mercado Pago...</p>",
-                icon: "info",
-                showConfirmButton: false,
-                allowOutsideClick: false,
-                allowEscapeKey: false,
-                allowEnterKey: false,
-            });
-
-
-
-            const mercadoPagoWindow = window.open(`https://www.mercadopago.com.pe/checkout/v1/redirect?preference_id=${id}`, '_blank');
-            
-            const checkPaymentStatus = setInterval(async () => {
-                if (mercadoPagoWindow.closed) {
-                    clearInterval(checkPaymentStatus);
-                    // Verificar el estado del pago aquí, por ejemplo, haciendo una solicitud al servidor
-                    const paymentStatus = await verificarEstadoDePago(preferenceId);
-                    if (paymentStatus === 'approved') {
-                                         
-                        await dispatch(viajeConfirmado(infoAmandarAlBack));
-                        
-                        Swal.fire({
-                            title: "Viaje reservado",
-                            text: "El pago se ha completado con éxito.",
-                            icon: "success"
-                        });
-                    } else {
-                        // El pago no fue aprobado, puedes manejarlo de acuerdo a tus necesidades
-                        console.log('El pago no fue aprobado');
-                    }
-                }
-            }, 1000); // Puedes ajustar el intervalo según tus necesidades
-        
-        }
-    };
-
-    useEffect(() => {
-        if (infoConfirmacionViaje.id) {
-            const infoAmandarAlBack={
-                tripId:infoConfirmacionViaje.id,
-                userId:infoConfirmacionViaje.userId
+        const handlePayment = async () => {
+            const id = await createPreference();
+            if (id) {
+              // Redirigir a la página de pago de MercadoPago
+              window.location.href = `https://www.mercadopago.com.ar/checkout/v1/redirect?preference_id=${id}`;
             }
+          };
 
-            Swal.fire({
+          useEffect(() => {
+            if (infoConfirmacionViaje.id) {
+              const infoAmandarAlBack = {
+                tripId: infoConfirmacionViaje.id,
+                userId: infoConfirmacionViaje.userId
+              }
+        
+              Swal.fire({
                 title: "Confirmación de traslado",
                 html: renderToString(confirmationText),
                 icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#3085d6",
                 cancelButtonColor: "#d33",
-                confirmButtonText: "Reservar y pagar",
+                confirmButtonText: "Pagar con MercadoPago",
                 showClass: {
-                    popup: 'animate__animated animate__fadeInDown',
+                  popup: 'animate__animated animate__fadeInDown',
                 },
                 hideClass: {
-                    popup: 'animate__animated animate__fadeOutUp',
+                  popup: 'animate__animated animate__fadeOutUp',
                 },
                 preConfirm: async () => {
-                    // Aquí manejas la lógica de reservar y pagar con Mercado Pago
-                    await handlePayment();
+                  // Aquí manejas la lógica de reservar y pagar con Mercado Pago
+                  await handlePayment();
                 }
-            })
-    
-            }    
-
-     }, [infoConfirmacionViaje, dispatch, confirmationText]/* [dispatch, input, handleConfirmation] */);
-
-      
-
-
-
-    const handleSubmit=async(event)=>{
-        event.preventDefault();
-        await setInput({
-            ...input,
-            /* userId: "3027b2fa-4997-4068-9f6d-c847baa02291" */
-        })
-        /* const newInput={
-            ...input,
-            
-        } */
-        /* setInput(newInput); */
-        console.log(input)
-        await dispatch(postNewViaje(input));
-        /* try {
-            const confirmedData = await dispatch(postNewViaje(input));
-    
-            if (confirmedData.id) {
-                handleConfirmation(confirmedData);
-            } else {
-                // Mostrar mensaje de error si la confirmación no tiene el formato esperado
-                console.error('Error: El objeto confirmado no tiene el formato esperado');
+              });
             }
-        } catch (error) {
-            // Mostrar mensaje de error si hay un problema con la solicitud
-            console.error('Error en la solicitud:', error.message);
-        } */
-        
-    }
+          }, [infoConfirmacionViaje, dispatch, confirmationText]);
+        ;
 
-    const handleChange=async(e)=>{
+
+
+
+
+        const handleSubmit = async (event) => {
+            event.preventDefault();
+            setInput({
+              ...input,
+            })
+            console.log(input)
+            await dispatch(postNewViaje(input));
+          }
         
-        setInput({
-            ...input,
-            [e.target.name]:e.target.value
-        })
-    }
-  
+          const handleChange = async (e) => {
+            setInput({
+              ...input,
+              [e.target.name]: e.target.value
+            })
+          }
+
     return (
-  
-      <div >
-        <form onSubmit={handleSubmit}>
-            <Stack spacing={4} bg='gray.100' p='5' h='auto' borderRadius='20' boxShadow='dark-lg' >
-                <Heading>Datos del viaje</Heading>
-                <Box>
-                    <Center py={2} gap={4} >
-                        
-                        <FormControl isRequired>
-                            <FormLabel>Desde</FormLabel>
-                            <Select placeholder='Selecciona el origen' name='origin' onChange={handleChange}>
-                                <option>Aeropuerto Talara</option>
-                                <option>Aeropuerto Tumbes</option>
-                                <option>Zona 1</option>
-                                <option>Zona 2</option>
-                                <option>Zona 3</option>
-                                <option>Zona 4</option>
-                            </Select>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Hasta</FormLabel>
-                            <Select placeholder='Selecciona el destino' name='destination' onChange={handleChange}>
-                            <option>Aeropuerto Talara</option>
-                                <option>Aeropuerto Tumbes</option>
-                                <option>Zona 1</option>
-                                <option>Zona 2</option>
-                                <option>Zona 3</option>
-                                <option>Zona 4</option>
-                            </Select>
-                        </FormControl>
-                    </Center>
 
-                    <Center py={2} gap={4} >
-                        <FormControl isRequired>
-                            <FormLabel>Día de recojida</FormLabel>
-                            <Input
-                                placeholder="Select Date and Time"
-                                size="md"
-                                type="date"
-                                name='date'
-                                value={input.date}
-                                onChange={handleChange} />
-                        </FormControl>
+        <div >
+            <form onSubmit={handleSubmit}>
+                <Stack spacing={4} bg='gray.100' p='5' h='auto' borderRadius='20' boxShadow='dark-lg' >
+                    <Heading>Datos del viaje</Heading>
+                    <Box>
+                        <Center py={2} gap={4} >
 
+                            <FormControl isRequired>
+                                <FormLabel>Desde</FormLabel>
+                                <Select placeholder='Selecciona el origen' name='origin' onChange={handleChange}>
+                                    <option>Aeropuerto Talara</option>
+                                    <option>Aeropuerto Tumbes</option>
+                                    <option>Zona 1</option>
+                                    <option>Zona 2</option>
+                                    <option>Zona 3</option>
+                                    <option>Zona 4</option>
+                                </Select>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Hasta</FormLabel>
+                                <Select placeholder='Selecciona el destino' name='destination' onChange={handleChange}>
+                                    <option>Aeropuerto Talara</option>
+                                    <option>Aeropuerto Tumbes</option>
+                                    <option>Zona 1</option>
+                                    <option>Zona 2</option>
+                                    <option>Zona 3</option>
+                                    <option>Zona 4</option>
+                                </Select>
+                            </FormControl>
+                        </Center>
 
-                        <FormControl isRequired>
-                            <FormLabel>Hora de recojida</FormLabel>
-                            <Input 
-                                type='time' 
-                                placeholder='Hora' 
-                                name='hour'  
-                                value={input.hour}
-                                onChange={handleChange} />
-                        </FormControl>
-                    </Center>
+                        <Center py={2} gap={4} >
+                            <FormControl isRequired>
+                                <FormLabel>Día de recojida</FormLabel>
+                                <Input
+                                    placeholder="Select Date and Time"
+                                    size="md"
+                                    type="date"
+                                    name='date'
+                                    value={input.date}
+                                    onChange={handleChange} />
+                            </FormControl>
 
 
-                    <Center py={2} gap={4}>
-                        <FormControl as='fieldset' isRequired>
-                            <FormLabel htmlFor='pasajeros'>Cantidad de pasajeros</FormLabel>
-                            <Select color='#000' placeholder='Cantidad de pasajeros' id='pasajeros' name='quantityPassengers'  onChange={handleChange} >
-                                {[...Array(20).keys()].map((number) => (
-                                    <option key={number + 1} id={`number-${number + 1}`} value={number + 1}>
-                                        {number + 1}
-                                    </option>
-                                ))}
-                            </Select>
-                        </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel>Hora de recojida</FormLabel>
+                                <Input
+                                    type='time'
+                                    placeholder='Hora'
+                                    name='hour'
+                                    value={input.hour}
+                                    onChange={handleChange} />
+                            </FormControl>
+                        </Center>
 
-                <Box mt={4}>
-                    <Button colorScheme='teal' variant='outline' w='100%' type='submit'>
-                        Reservar viaje</Button>
-                </Box>
-                    </Center>
-                </Box>
-            </Stack>
-        </form>
-      </div>
-      
+
+                        <Center py={2} gap={4}>
+                            <FormControl as='fieldset' isRequired>
+                                <FormLabel htmlFor='pasajeros'>Cantidad de pasajeros</FormLabel>
+                                <Select color='#000' placeholder='Cantidad de pasajeros' id='pasajeros' name='quantityPassengers' onChange={handleChange} >
+                                    {[...Array(20).keys()].map((number) => (
+                                        <option key={number + 1} id={`number-${number + 1}`} value={number + 1}>
+                                            {number + 1}
+                                        </option>
+                                    ))}
+                                </Select>
+                            </FormControl>
+
+                            <Box mt={4}>
+                                <Button colorScheme='teal' variant='outline' w='100%' type='submit'>
+                                    Reservar viaje</Button>
+                            </Box>
+                        </Center>
+                    </Box>
+                </Stack>
+            </form>
+        </div>
+
     )
-  }
-  
-  export default SolicitudViajeForm
+}
+
+export default SolicitudViajeForm
