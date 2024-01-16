cd8c0455a4a4dcf1d9077352d8705ada1c18ca05
rubencorba <rubencorba@gmail.com>
2024-01-15 22:51:37 -0300

    flujo de confirmacion de viaje5

--- front/src/views/Forms/SolicitudViaje/SolicitudViajeForm.jsx
+++ front/src/views/Forms/SolicitudViaje/SolicitudViajeForm.jsx
@@ -41,36 +41,37 @@ function SolicitudViajeForm() {
         </div>
     );
 
+
     useEffect(() => {
         if (infoConfirmacionViaje.id) {
             const infoAmandarAlBack={
                 tripId:infoConfirmacionViaje.id,
                 userId:infoConfirmacionViaje.userId
             }
-        
-        Swal.fire({
-            title: "Confirmación de traslado",
-            html: renderToString(confirmationText),
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Mercado Pago",
-            htmlMode: true
-          }).then(async(result) => {
-            if (result.isConfirmed) {
-                await dispatch(viajeConfirmado(infoAmandarAlBack)) //Agregado para guardar viaje en DB
-              Swal.fire({
-                title: "Viaje reservado",
-                text: "Simulando que se abonó..",
-                icon: "success"
+
+            Swal.fire({
+                title: "Confirmación de traslado",
+                html: renderToString(confirmationText),
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Mercado Pago",
+                htmlMode: true
+              }).then(async(result) => {
+                if (result.isConfirmed) {
+                    await dispatch(viajeConfirmado(infoAmandarAlBack)) //Agregado para guardar viaje en DB
+                  Swal.fire({
+                    title: "Viaje reservado",
+                    text: "Simulando que se abonó..",
+                    icon: "success"
+                  });
+                }
               });
-            }
-          });
+    
+            }    
 
-        }
-
-     }, [infoConfirmacionViaje/* , dispatch, confirmationText */]);
+     }, [infoConfirmacionViaje, dispatch, confirmationText]/* [dispatch, input, handleConfirmation] */);
 
       
 
@@ -78,16 +79,30 @@ function SolicitudViajeForm() {
 
     const handleSubmit=async(event)=>{
         event.preventDefault();
-        setInput({
+        await setInput({
             ...input,
+            /* userId: "3027b2fa-4997-4068-9f6d-c847baa02291" */
         })
-        const newInput={
+        /* const newInput={
             ...input,
-            userId: "3027b2fa-4997-4068-9f6d-c847baa02291"
-        }
-        setInput(newInput);
+            
+        } */
+        /* setInput(newInput); */
         console.log(input)
         await dispatch(postNewViaje(input));
+        /* try {
+            const confirmedData = await dispatch(postNewViaje(input));
+    
+            if (confirmedData.id) {
+                handleConfirmation(confirmedData);
+            } else {
+                // Mostrar mensaje de error si la confirmación no tiene el formato esperado
+                console.error('Error: El objeto confirmado no tiene el formato esperado');
+            }
+        } catch (error) {
+            // Mostrar mensaje de error si hay un problema con la solicitud
+            console.error('Error en la solicitud:', error.message);
+        } */
         
     }
 
