1a8d1792ec245b45c997cdb63868a5ed93d82472
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-01-16 16:27:57 -0300

    Se agrego la posibilidad de buscar reviews de un chofer por id

--- Back/src/Handlers/ReviewsHandlers/getReviewsHandler.js
+++ Back/src/Handlers/ReviewsHandlers/getReviewsHandler.js
@@ -2,8 +2,14 @@ const getReviews = require('../../Controllers/ReviewsControllers/getReviews');
 
 module.exports=async(req,res)=>{
     try{
-        const allReviews=await getReviews();
+        const { driverId } = req.body;
+        
+        let allReviews=await getReviews();
  
+        //Si se recibe un id de conductor, se retornan las reviews de este chofer.
+        if(driverId)
+            allReviews=allReviews?.filter(rev=>rev.driverId===driverId);
+
         res.status(200).json(allReviews);
     }catch(error){
         res.status(400).json({error: error.message});
