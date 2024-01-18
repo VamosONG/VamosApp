ea095afec4189528548360de428029cf7f2dc4b8
Fishlennon <jpaviles5@gmail.com>
2024-01-18 00:22:42 -0300

    d

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Agrega un alias para resolver la ruta del archivo VAMOS.png
-      '/assets/VAMOS.png': path.resolve(__dirname, 'src/assets/VAMOS.png'),
-    },
-  },
+  
 })
