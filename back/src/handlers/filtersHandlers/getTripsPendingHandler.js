8f4883ea77e3e6cb0066c4e02c6cb9a2ee4a90b7
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-01-19 19:48:56 -0300

    Se agrego prop driverFullName en el listado de viajes PENDIENTES

--- back/src/handlers/filtersHandlers/getTripsPendingHandler.js
+++ back/src/handlers/filtersHandlers/getTripsPendingHandler.js
@@ -1,4 +1,5 @@
 const {getTrips} = require('../../controllers/tripsControllers/getTrips');
+const getDriverById = require('../../controllers/driversControllers/getDriverById');
 
 module.exports = async (req, res) => {
     try {
@@ -6,7 +7,18 @@ module.exports = async (req, res) => {
         const allTrips = await getTrips();
         const pendingTrips=allTrips.filter(res=>res.stateOfTrip==='pending')
 
-        res.status(200).json(pendingTrips);
+        const mapeo = await Promise.all(pendingTrips.map(async tr=>{
+            const {dataValues, driverFullName} = tr;
+            const chofer=await getDriverById(tr.driverId);
+            const newTrip={
+                ...dataValues,
+                driverFullName: `${chofer.name} ${chofer.surname}`
+            }
+
+            return newTrip;
+        }));
+
+        res.status(200).json(mapeo);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
