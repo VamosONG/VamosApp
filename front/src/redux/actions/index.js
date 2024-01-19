442f80db3e59477388388736af0bc17b0197a8be
unknown <rocio.sosa94@gmail.com>
2024-01-18 22:37:31 -0300

    registro funcionando con firebase

--- front/src/redux/actions/index.js
+++ front/src/redux/actions/index.js
@@ -1,41 +1,41 @@
-import axios from "axios";
-import choferes from "../../utils/chofer";
+import axios from 'axios';
+import choferes from '../../utils/chofer'
 
-import { DELETE_DRIVER,GET_TRIP_ID, FILTER_AIRPORT, FILTER_CAR, ORDER_ALPHABETICAL, ORDER_PASSENGER, ORDER_RATING, UPDATE_DRIVER_DATA } from "./action.types";
-
+import { DELETE_DRIVER,GET_TRIP_ID, FILTER_AIRPORT, FILTER_CAR, ORDER_ALPHABETICAL, ORDER_PASSENGER, ORDER_RATING, UPDATE_DRIVER_DATA } from './action.types';
 
 //Estas constantes deben ir enotro activo llamado ACTION.TYPES.JS
-export const PAGINATE = "PAGINATE";
-export const GET_ALL_CONDUCTORES = "GET_ALL_CONDUCTORES";
-export const POST_NEW_VIAJE = "POST_NEW_VIAJE";
-export const LOGIN = "LOGIN";
-export const ID_SOLICITUD = "ID_SOLICITUD";
-export const GET_RESERVED_TRIPS = "GET_RESERVED_TRIPS";
-export const GET_PENDING_TRIPS = "GET_PENDING_TRIPS";
+export const PAGINATE = "PAGINATE"
+export const GET_ALL_CONDUCTORES = "GET_ALL_CONDUCTORES"
+export const POST_NEW_VIAJE = "POST_NEW_VIAJE"
+export const LOGIN = "LOGIN"
+export const ID_SOLICITUD = "ID_SOLICITUD"
+export const GET_RESERVED_TRIPS = "GET_RESERVED_TRIPS"
+export const GET_PENDING_TRIPS = "GET_PENDING_TRIPS"
 export const GET_COMPLETED_TRIPS = "GET_COMPLETED_TRIPS"
-export const NEW_USER = "NEW_USER";
-export const LOGOUT = "LOGOUT";
-export const CREATE_CHOFER = "CREATE_CHOFER";
-export const VIAJE_CONFIRMADO = "VIAJE_CONFIRMADO";
-export const GET_FILTERED = "GET_FILTERED";export const GET_TRIPS_BY_ID = 'GET_TRIPS_BY_ID'
+export const NEW_USER = 'NEW_USER'
+export const LOGOUT = 'LOGOUT'
+export const CREATE_CHOFER = 'CREATE_CHOFER'
+export const VIAJE_CONFIRMADO = 'VIAJE_CONFIRMADO'
+export const GET_FILTERED = 'GET_FILTERED'
+export const GET_TRIPS_BY_ID = 'GET_TRIPS_BY_ID'
 export const POST_REVIEW = 'POST_REVIEW'
 
-
 export const getAllConductores = () => {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios(`http://localhost:3001/drivers`);
-      dispatch({
-        type: GET_ALL_CONDUCTORES,
-        payload: data,
-      });
-      return data;
-    } catch (error) {
-      console.error("Error al obtener conductores:", error);
-      throw error;
+    return async (dispatch) => {
+        try {
+            const { data } = await axios(`http://localhost:3001/drivers`);
+            dispatch({
+                type: GET_ALL_CONDUCTORES,
+                payload: data
+            });
+            return data
+        } catch (error) {
+            console.error('Error al obtener conductores:', error);
+            throw error;
+        }
     }
-  };
-};
+
+}
 
 export const getTripById = (id) => {
     return async (dispatch) => {
@@ -52,35 +52,33 @@ export const getTripById = (id) => {
 }
 
 export const createNewChofer = (data) => {
-  console.log("data en actiopn" + data);
-  return async (dispatch) => {
-    try {
-      return dispatch({
-        type: CREATE_CHOFER,
-        payload: data,
-      });
-    } catch (error) {
-      alert("¡Error en la craecion de chofer!");
-      throw new Error(error);
+    console.log('data en actiopn' + data);
+    return async (dispatch) => {
+        try {
+            return dispatch({
+                type: CREATE_CHOFER,
+                payload: data
+            })
+        } catch (error) {
+            alert('¡Error en la craecion de chofer!');
+            throw new Error(error);
+        }
     }
-  };
-};
+}
 
 export const deleteDriverAction = (id) => {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.delete(
-        `http://localhost:3001/drivers/${id}`
-      );
-      dispatch({
-        type: DELETE_DRIVER,
-        payload: data,
-      });
-    } catch (error) {
-      console.error(error);
+    return async (dispatch) => {
+        try {
+            const {data} = await axios.delete(`http://localhost:3001/drivers/${id}`)
+            dispatch({
+                type: DELETE_DRIVER,
+                payload: data
+            })
+        } catch (error) {
+            console.error(error);
+        }
     }
-  };
-};
+}
 
 // export const updateDriverData = (id, newData) => {
 //     return async (dispatch) => {
@@ -94,27 +92,15 @@ export const deleteDriverAction = (id) => {
 //         }
 //     }
 // }
-export const updateDriverData = (id, newData) => {
-  return async (dispatch) => {
-    try {
-      dispatch({
-        type: UPDATE_DRIVER_DATA,
-        payload: data,
-      });
-    } catch (error) {
-      console.error({ message: "Error en action", error });
-    }
-  };
-};
 
 export const postNewViaje = (infoViaje) => {
-  infoViaje.userId = "3027b2fa-4997-4068-9f6d-c847baa02291";
+
+    infoViaje.userId= "3027b2fa-4997-4068-9f6d-c847baa02291"
 
     return async (dispatch) => {
         try {
 
             /* const { data } = await axios.post(`https://vamosappserver.onrender.com/offer/create`, infoViaje); */
-
 
             const { data } = await axios.post(`http://localhost:3001/offer/create`, infoViaje);
             console.log(data)
@@ -131,55 +117,37 @@ export const postNewViaje = (infoViaje) => {
     }
 }
 
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.post(
-        `http://localhost:3001/offer/create`,
-        infoViaje
-      );
-      console.log(data);
-      await dispatch({
-        type: POST_NEW_VIAJE,
-        payload: data,
-      });
-    } catch (error) {
-      window.alert("¡Error en la solicitud!");
-      /* throw new Error(error); */
-      console.log(error.message);
+export const getReservedTrips = () =>{
+    return async(dispatch)=> {
+        const endpoint= 'http://localhost:3001/trips/reserves' //Se cambió a la ruta con viajes reservados
+        try {
+            const { data } = await axios.get(endpoint)
+            console.log(data);
+            return dispatch({
+                type: GET_RESERVED_TRIPS,
+                payload: data
+            })
+        } catch (error) {
+            console.log(error);
+            alert("error")
+        }
     }
-
-
-export const getReservedTrips = () => {
-  return async (dispatch) => {
-    const endpoint = "http://localhost:3001/trips/reserves"; //Se cambió a la ruta con viajes reservados
-    try {
-      const { data } = await axios.get(endpoint);
-      console.log(data);
-      return dispatch({
-        type: GET_RESERVED_TRIPS,
-        payload: data,
-      });
-    } catch (error) {
-      console.log(error);
-      alert("error");
+}
+export const getPendingTrips = () =>{
+    return async(dispatch)=> {
+        const endpoint= 'http://localhost:3001/trips/pending' //ruta con viajes pendientes
+        try {
+            const { data } = await axios.get(endpoint)
+            console.log(data);
+            return dispatch({
+                type: GET_PENDING_TRIPS,
+                payload: data
+            })
+        } catch (error) {
+            console.log(error);
+            alert("error")
+        }
     }
-  };
-};
-export const getPendingTrips = () => {
-  return async (dispatch) => {
-    const endpoint = "http://localhost:3001/trips/pending"; //ruta con viajes pendientes
-    try {
-      const { data } = await axios.get(endpoint);
-      console.log(data);
-      return dispatch({
-        type: GET_PENDING_TRIPS,
-        payload: data,
-      });
-    } catch (error) {
-      console.log(error);
-      alert("error");
-    }
-  }
 }
 export const getCanceledTrips = () =>{
     return async(dispatch)=> {
@@ -187,15 +155,6 @@ export const getCanceledTrips = () =>{
         try {
             const { data } = await axios.get(endpoint)
             console.log(data);
-  };
-};
-}
-/* export const getAllConductores=()=>{
-    try {
-
-        return async (dispatch)=>{
-            const data= choferes
-            console.log(data)
             return dispatch({
                 type: GET_COMPLETED_TRIPS,
                 payload: data
@@ -207,125 +166,114 @@ export const getCanceledTrips = () =>{
     }
 }
 
-// export const postNewUser = (form) => {
-//   console.log(form);
-//   const endpoint = `http://localhost:3001/user/create`;
-//   return async (dispatch) => {
-//     try {
-//       const { data } = await axios.post(endpoint, form);
-//       console.log(data); // ACA NO RECIBE LA DATA 
-//       alert("usuario creado");
-//       return await dispatch({
-//         type: NEW_USER,
-//         payload: data,
-//       });
-//     } catch (error) {
-//       console.error(`Error en la creacion del usuario:${error.message}`);
-//       alert("Error")
-//       throw error;
-//     }
-//   };
-// };
+export const postNewUser = (form) => {
+    console.log(form);
+    return async (dispatch) => {
+        try {
+            return dispatch({
+                type: NEW_USER,
+                payload: form
+            })
+        } catch (error) {
+            console.error("Error en la creacion del usuario:", error);
+            throw error;
+        }
+    }
+
+}
 
 export const paginateConductores = (order) => {
-  return async (dispatch) => {
-    try {
-      dispatch({
-        type: PAGINATE,
-        payload: order,
-      });
-    } catch (error) {
-      /* throw new Error(error.response.data.error); */
-      //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
-    }
-  };
+
+    return async (dispatch) => {
+        try {
+
+            dispatch({
+                type: PAGINATE,
+                payload: order
+            })
+        } catch (error) {
+            /* throw new Error(error.response.data.error); */  //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
+        };
+    };
 };
 
 export const logIn = (input) => {
-  return async (dispatch) => {
-    try {
-      dispatch({
-        type: LOGIN,
-        payload: input,
-      });
-    } catch (error) {
-      /* throw new Error(error.response.data.error); */
-      //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
-    }
-  };
+
+    return async (dispatch) => {
+        try {
+            dispatch({
+                type: LOGIN,
+                payload: input
+            })
+        } catch (error) {
+            /* throw new Error(error.response.data.error); */  //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
+        };
+    };
 };
 
 export const idDeSolicitud = (id) => {
-  return {
-    type: ID_SOLICITUD,
-    payload: id,
-  };
+    return ({
+        type: ID_SOLICITUD,
+        payload: id
+    })
+
 };
 
 export const logOutAction = (value) => {
-  return {
-    type: LOGOUT,
-    payload: value,
-  };
-};
+    return {
+        type: LOGOUT,
+        payload: value
+    };
+}
 
 export const viajeConfirmado = (info) => {
-  console.log(info);
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.put(
-        `http://localhost:3001/trips/reserves/create`,
-        info
-      );
-      console.log(data);
-      dispatch({
-        type: VIAJE_CONFIRMADO,
-        payload: data,
-      });
-    } catch (error) {
-      /* throw new Error(error.response.data.error); */ //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
-      console.log(error.message);
-    }
-  };
+    console.log(info)
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.put(`http://localhost:3001/trips/reserves/create`, info);
+            console.log(data)
+            dispatch({
+                type: VIAJE_CONFIRMADO,
+                payload: data
+            })
+        } catch (error) {
+            /* throw new Error(error.response.data.error); */  //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
+            console.log(error.message)
+        };
+    };
 };
 
 export const filtrarConductores = (info) => {
-  console.log(info);
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.post(
-        `http://localhost:3001/drivers/filter`,
-        info
-      );
-      console.log(data);
-      dispatch({
-        type: GET_FILTERED,
-        payload: data,
-      });
-    } catch (error) {
-      /* throw new Error(error.response.data.error); */ //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
-      console.log(error.message);
-    }
-  };
+    console.log(info)
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.post(`http://localhost:3001/drivers/filter`, info);
+            console.log(data)
+            dispatch({
+                type: GET_FILTERED,
+                payload: data
+            })
+        } catch (error) {
+            /* throw new Error(error.response.data.error); */  //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
+            console.log(error.message)
+        };
+    };
 };
 export const conductorAsignado = (info) => {
-  console.log(info);
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.put(
-        `http://localhost:3001/trips/reserves/update`,
-        info
-      );
-      console.log(data);
-      /* dispatch({
+    console.log(info)
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.put(`http://localhost:3001/trips/reserves/update`, info);
+            console.log(data)
+            /* dispatch({
                 type: GET_FILTERED,
                 payload: data
             }) */
-    } catch (error) {
-      /* throw new Error(error.response.data.error); */ //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
-      console.log(error.message);
-    }
-  };
+        } catch (error) {
+            /* throw new Error(error.response.data.error); */  //COMENTADO HASTA QUE RECIBA ALGO DEL BACK
+            console.log(error.message)
+        };
+    };
 };
 
 export const alphabeticalOrder = (order) => {
@@ -353,7 +301,6 @@ export const passengerOrder = (data) => {
         }
     }
 }
-
 
 export const ratingOrder = (data) => {
     return async (dispatch) => {
@@ -418,6 +365,7 @@ export const postReview = (info) => {
         }
     }
 }
+
 
 
 
--- front/src/redux/reducer/index.js
+++ front/src/redux/reducer/index.js
@@ -1,250 +1,208 @@
-import {
-  DELETE_DRIVER,
-  GET_TRIP_ID,
-  FILTER_AIRPORT,
-  FILTER_CAR,
-  ORDER_ALPHABETICAL,
-  ORDER_PASSENGER,
-  ORDER_RATING,
-} from "../actions/action.types";
-import {
-  CREATE_CHOFER,
-  GET_ALL_CONDUCTORES,
-  GET_TRIPS_BY_ID,
-  GET_COMPLETED_TRIPS,
-  GET_FILTERED,
-  GET_PENDING_TRIPS,
-  GET_RESERVED_TRIPS,
-  ID_SOLICITUD,
-  LOGIN,
-  LOGOUT,
-  NEW_USER,
-  PAGINATE,
-  POST_NEW_VIAJE,
-} from "../actions/index";
+import { DELETE_DRIVER,GET_TRIP_ID, FILTER_AIRPORT, FILTER_CAR, ORDER_ALPHABETICAL, ORDER_PASSENGER, ORDER_RATING } from "../actions/action.types";
+import { CREATE_CHOFER, GET_ALL_CONDUCTORES, GET_TRIPS_BY_ID, GET_COMPLETED_TRIPS, GET_FILTERED, GET_PENDING_TRIPS, GET_RESERVED_TRIPS, ID_SOLICITUD, LOGIN, LOGOUT, NEW_USER, PAGINATE, POST_NEW_VIAJE } from "../actions/index";
+
+
 
 const initialState = {
-  allData: [], //Este estado servidara para mantener la data general de la base de datos
-  conductores: [],
-  pageConductores: [],
-  currentPage: 0,
-  newUsuario: [],
+    
+    allData: [],//Este estado servidara para mantener la data general de la base de datos
+    conductores: [],
+    pageConductores: [],
+    currentPage: 0,
+    newUsuario: [],
 
-  esAdmin: false,
-  esUsuario: false,
+    esAdmin: false,
+    esUsuario: false,
 
-  viajesReservados: [],
-  viajesPendientes: [],
-  viajesCompletados: [],
-  idSolicitud: "",
+    viajesReservados: [],
+    viajesPendientes: [],
+    viajesCompletados: [],
+    idSolicitud: '',
 
-  infoConfirmacionViaje: {},
+    infoConfirmacionViaje:{},
 
-  conductoresFiltrados: [],
+    conductoresFiltrados:[],
 
-  tripsById: [],
+    tripsById:[],
 
-  trip: []
-};
+    trip:[]
+
+}
 
 const reducer = (state = initialState, action) => {
-  const PAGE_DATA = 6;
-  switch (action.type) {
-    case GET_ALL_CONDUCTORES:
-      return {
-        ...state,
-        conductores: action.payload,
-        pageConductores: action.payload,
-        allData: action.payload,
-      };
-    case GET_TRIP_ID:
-      return {
-        ...state,
-        trip: action.payload,
-      };
-    case DELETE_DRIVER:
-      return {
-        ...state,
-        conductores: state.conductores.filter(
-          (driver) => driver.id !== action.payload
-        ),
-      };
-    case PAGINATE:
-      const nextPage = state.currentPage + 1;
-      const prevPage = state.currentPage - 1;
-      const firstIndex =
-        action.payload === "next"
-          ? nextPage * state.cantConductoresPorPag
-          : prevPage * state.cantConductoresPorPag;
-      // console.log(action.payload)
-      // console.log(firstIndex)
-      // console.log(nextPage)
+    const PAGE_DATA = 6;
+    switch (action.type) {
+        case GET_ALL_CONDUCTORES:
+            return {
+                ...state,
+                conductores: action.payload,
+                pageConductores: action.payload,
+                allData: action.payload            };
+        case GET_TRIP_ID:
+            return{
+                ...state,
+                trip:action.payload
+            }
+        case DELETE_DRIVER:
+            return {
+                ...state, 
+                conductores: state.conductores.filter(driver => driver.id !== action.payload)
+            }
+        case PAGINATE:
+            const nextPage = state.currentPage + 1;
+            const prevPage = state.currentPage - 1;
+            const firstIndex = action.payload === "next" ? nextPage * state.cantConductoresPorPag : prevPage * state.cantConductoresPorPag;
+            // console.log(action.payload)
+            // console.log(firstIndex)
+            // console.log(nextPage)
 
-      /* if (action.payload === 'next' && firstIndex >= state.pageConductores.length) {
+            /* if (action.payload === 'next' && firstIndex >= state.pageConductores.length) {
                 return state
             } else if (action.payload === "prev" && prevPage < 0) {
                 return state
             }; */
 
-      return {
-        ...state,
-        pageConductores:
-          /* [...state.pageConductores] */ state.conductores.splice(
-            firstIndex,
-            state.cantConductoresPorPag
-          ),
-        currentPage: action.payload === "next" ? nextPage : prevPage,
-      };
+            return {
+                ...state,
+                pageConductores: /* [...state.pageConductores] */state.conductores.splice(firstIndex, state.cantConductoresPorPag),
+                currentPage: action.payload === "next" ? nextPage : prevPage,
+            };
 
-    case LOGIN:
-      if (action.payload.email === "asd" && action.payload.password === "123") {
-        return {
-          ...state,
-          esAdmin: true,
-        };
-      } else {
-        return {
-          ...state,
-          esUsuario: true,
-        };
-      }
+        case LOGIN:
+            if (action.payload.email === 'asd' && action.payload.password === '123') {
+                return {
+                    ...state,
+                    esAdmin: true
+                }
+            } else {
+                return {
+                    ...state,
+                    esUsuario: true
+                }
+            }
 
-    //BTN de salida o cerrar sesion
-    case LOGOUT:
-      const value = action.payload;
+        //BTN de salida o cerrar sesion
+        case LOGOUT:
+            const value = action.payload;
 
-      if (value === "admin") {
-        return {
-          ...state,
-          esAdmin: false,
-        };
-      } else if (value === "user") {
-        return {
-          ...state,
-          esUsuario: false,
-        };
-      }
+            if (value === 'admin') {
+                return {
+                    ...state,
+                    esAdmin: false
+                }
+            } else if (value === 'user') {
+                return {
+                    ...state,
+                    esUsuario: false
+                }
+            }
+        //Creando un nuevo usuario
+        case NEW_USER:
+            return {
+                ...state,
+                newUsuario: [action.payload]
+            }
 
-    //Creando un nuevo usuario
-    // case NEW_USER:
-    //     console.log(action.payload);
-    //     return {
-    //         ...state,
-    //         newUsuario: [...state.newUsuario, action.payload]
-    //     }
+        case POST_NEW_VIAJE: 
+            console.log(action.payload)
+            return {
+            ...state,
+            infoConfirmacionViaje: action.payload
+        }
+        
+        case GET_RESERVED_TRIPS:
+            return {
+                ...state,
+                viajesReservados: action.payload
+            }
+        case GET_PENDING_TRIPS:
+            return {
+                ...state,
+                viajesPendientes: action.payload
+            }
+        case GET_COMPLETED_TRIPS:
+            return {
+                ...state,
+                viajesCompletados: action.payload
+            }
 
-    case POST_NEW_VIAJE:
-      console.log(action.payload);
-      return {
-        ...state,
-        infoConfirmacionViaje: action.payload,
-      };
+        case ID_SOLICITUD:
+            return {
+                ...state,
+                idSolicitud: action.payload
+            }
+        case GET_FILTERED:
+            return {
+                ...state,
+                conductoresFiltrados: action.payload,
+            }
+        case ORDER_ALPHABETICAL:
+            let orderedList = [...state.conductores];
+            if (action.payload === "A") {
+                orderedList.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
+            } else if (action.payload === "D") {
+                orderedList.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1);
+            }
+            return {
+                ...state,
+                conductores: orderedList,
+                pageConductores: orderedList
+            }
 
-    case GET_RESERVED_TRIPS:
-      return {
-        ...state,
-        viajesReservados: action.payload,
-      };
-    case GET_PENDING_TRIPS:
-      return {
-        ...state,
-        viajesPendientes: action.payload,
-      };
-    case GET_COMPLETED_TRIPS:
-      return {
-        ...state,
-        viajesCompletados: action.payload,
-      };
+        case ORDER_PASSENGER:
+            let orderedListPassenger = [...state.conductores];
+            if (action.payload === 'A') {
+                orderedListPassenger.sort((a,b) => a.capacityPassengers < b.capacityPassengers ? 1 : -1)
+            } else if (action.payload === 'D') {
+                orderedListPassenger.sort((a,b) => a.capacityPassengers > b.capacityPassengers ? 1 : -1)
+            }
+            return {
+                ...state,
+                conductores: orderedListPassenger,
+                pageConductores: orderedListPassenger
+            }
 
-    case ID_SOLICITUD:
-      return {
-        ...state,
-        idSolicitud: action.payload,
-      };
-    case GET_FILTERED:
-      return {
-        ...state,
-        conductoresFiltrados: action.payload,
-      };
-    case ORDER_ALPHABETICAL:
-      let orderedList = [...state.conductores];
-      if (action.payload === "A") {
-        orderedList.sort((a, b) =>
-          a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
-        );
-      } else if (action.payload === "D") {
-        orderedList.sort((a, b) =>
-          a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1
-        );
-      }
-      return {
-        ...state,
-        conductores: orderedList,
-        pageConductores: orderedList,
-      };
+        case ORDER_RATING:
+            let orderedListRating = [...state.conductores]
+            if (action.payload === 'A') {
+                orderedListRating.sort((a,b) => a.rating < b.rating ? 1 : -1)
+            } else if (action.payload === 'D') {
+                orderedListRating.sort((a,b) => a.rating > b.rating ? 1 : -1)
+            }
+            return {
+                ...state,
+                conductores: orderedListRating,
+                pageConductores: orderedListRating
+            }
 
-    case ORDER_PASSENGER:
-      let orderedListPassenger = [...state.conductores];
-      if (action.payload === "A") {
-        orderedListPassenger.sort((a, b) =>
-          a.capacityPassengers < b.capacityPassengers ? 1 : -1
-        );
-      } else if (action.payload === "D") {
-        orderedListPassenger.sort((a, b) =>
-          a.capacityPassengers > b.capacityPassengers ? 1 : -1
-        );
-      }
-      return {
-        ...state,
-        conductores: orderedListPassenger,
-        pageConductores: orderedListPassenger,
-      };
+        case FILTER_CAR:
+            const filterCarList = state.allData.filter((car) => car.carType === action.payload)
+            return {
+                ...state,
+                conductores: filterCarList,
+                pageConductores:filterCarList
+            }
 
-    case ORDER_RATING:
-      let orderedListRating = [...state.conductores];
-      if (action.payload === "A") {
-        orderedListRating.sort((a, b) => (a.rating < b.rating ? 1 : -1));
-      } else if (action.payload === "D") {
-        orderedListRating.sort((a, b) => (a.rating > b.rating ? 1 : -1));
-      }
-      return {
-        ...state,
-        conductores: orderedListRating,
-        pageConductores: orderedListRating,
-      };
+        case FILTER_AIRPORT:
+            const filterAirportList = state.allData.filter((zone) => zone.airports === action.payload)
 
-    case FILTER_CAR:
-      const filterCarList = state.allData.filter(
-        (car) => car.carType === action.payload
-      );
-      return {
-        ...state,
-        conductores: filterCarList,
-        pageConductores: filterCarList,
-      };
-
-    case FILTER_AIRPORT:
-      const filterAirportList = state.allData.filter(
-        (zone) => zone.airports === action.payload
-      );
-
-      return {
-        ...state,
-        conductores: [...filterAirportList],
-        pageConductores: filterAirportList,
-      };
-    case GET_TRIPS_BY_ID:
-      return {
-        ...state,
-        tripsById: action.payload,
-      };
-    /* case POST_REVIEW:
+            return {
+                ...state,
+                conductores: [...filterAirportList],
+                pageConductores: filterAirportList
+            }
+        case GET_TRIPS_BY_ID:
+            return {
+                ...state,
+                tripsById: action.payload
+            }
+        /* case POST_REVIEW:
             return {
                 ...state,
                 tripsById: action.payload
             } */
-    default:
-      return { ...state };
-  }
-};
-export default reducer;
+        default:
+            return { ...state };
+    }
+}
+export default reducer
