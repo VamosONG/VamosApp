952f11c4ee1345d42ee2409eabf81d0f0ff3c794
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-01-22 01:25:30 -0300

    Corregi error de validacion en postUser

--- back/src/controllers/usersControllers/postUser.js
+++ back/src/controllers/usersControllers/postUser.js
@@ -4,12 +4,14 @@ const { User } = require("../../dataBase")
 const postUser = async ({ name, surname, email, phone, dni }) => {
     try {
     
-        const [newUser, created] = await User.findOrCreate({ email },
-            {
+        const [newUser, created] = await User.findOrCreate({
+            where: { email },
+            defaults: {
                 name,
                 surname,
                 phone,
                 dni,
+            }
           });
         
         if(!created)
