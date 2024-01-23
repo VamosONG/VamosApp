467d32bf53f2f9b1b850857bc63b81394d1c2c8e
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-01-23 16:06:23 -0300

    Cambio de metodo createOffer (ya no crea un viaje al usuario) y correcciones en getTripsCompleted y getReserves

--- back/src/handlers/filtersHandlers/getTripsCompletedHandler.js
+++ back/src/handlers/filtersHandlers/getTripsCompletedHandler.js
@@ -1,6 +1,7 @@
 const {getTrips} = require('../../controllers/tripsControllers/getTrips');
 const updateTrip = require('../../controllers/tripsControllers/updateTrip');
 const getDriverById = require('../../controllers/driversControllers/getDriverById');
+const getUserById = require('../../controllers/usersControllers/getUserById');
 
 module.exports = async (req, res) => {
     try {
@@ -9,11 +10,12 @@ module.exports = async (req, res) => {
         const today = new Date();
 
         allTrips.forEach(viaje => {
-            if(viaje.date<today.toISOString().split('T')[0])
-                updateTrip(viaje.id, {stateOfTrip: 'completed'})
+            if(viaje.date<today.toISOString().split('T')[0]){
+                if(viaje.stateOfTrip==='pending')
+                    updateTrip(viaje.id, {stateOfTrip: 'completed'})
+            }
         });
         const completeTrips = allTrips?.filter(tr=>(tr.stateOfTrip==='completed') && (tr.driverId));
-
         const mapeo = await Promise.all(completeTrips?.map(async tr=>{
             const {dataValues, driverFullName, userEmail} = tr;
             const chofer=await getDriverById(tr.driverId);
