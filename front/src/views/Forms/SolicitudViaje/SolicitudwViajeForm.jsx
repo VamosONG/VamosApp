e01580e6aab721f465bc894e4be8a2b85bdd879b
rubencorba <rubencorba@gmail.com>
2024-01-27 21:51:04 -0300

    local storage funcando

--- front/src/views/Forms/SolicitudViaje/SolicitudwViajeForm.jsx
+++ front/src/views/Forms/SolicitudViaje/SolicitudwViajeForm.jsx
@@ -13,6 +13,7 @@ import { renderToString } from 'react-dom/server';
 import {  useNavigate } from 'react-router-dom';
 
 import { Field, Form, Formik } from 'formik';
+import { postNewViaje } from '../../../redux/actions';
 
 
 
@@ -23,25 +24,94 @@ const SolicitudwViajeForm = () => {
 
   const [input,setInput]=useState({})
 
+  const currentUserId = useSelector((state) => state.currentUser.id)
+  const infoConfirmacionViaje = useSelector((state) => state.infoConfirmacionViaje)
+
   useEffect(() => {
-    /* if (parsedData) {
-        setInput({
-            origin: parsedData.origin,
-            destination: parsedData.destination,
-            date: parsedData.date,
-            hour: parsedData.hour,
-            quantityPassengers: parsedData.quantityPassengers
-        });
-    } else {
-        setInput({
-            origin: "",
-            destination: "",
-            date: "",
-            hour: "",
-            quantityPassengers: ""
-        })
-    } */
+     const storedInput = localStorage.getItem('solicitudViajeInput');
+
+     if (storedInput) {
+        const parsedInput = JSON.parse(storedInput);
+        console.log(parsedInput)
+        // Utilizamos un efecto secundario para garantizar que setInput se haya completado antes de imprimir
+        const updateInput = () => {
+          setInput(parsedInput);
+          setOrigenSeleccionado(parsedInput.origin);
+          setDestinoSeleccionado(parsedInput.destination);
+          setHoraSeleccionada(parsedInput.hour);
+          setFechaSeleccionada(parsedInput.date);
+          setCantidadPasajerosSeleccionada(parsedInput.quantityPassengers);
+        };
+    
+        updateInput();
+      }
+    
   }, []);
+
+  useEffect(() => {
+     if (infoConfirmacionViaje.price) {
+        const confirmationText = (
+            <div>
+                <p>Origen: {infoConfirmacionViaje.origin}</p>
+                <p>Destino: {infoConfirmacionViaje.destination}</p>
+                <p>Cantidad de pasajeros: {infoConfirmacionViaje.quantityPassengers}</p>
+                <p>Precio final: {infoConfirmacionViaje.price}</p>
+            </div>
+        );
+
+
+        Swal.fire({
+            title: "Confirmación de traslado",
+            html: renderToString(confirmationText),
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Pagar con MercadoPago",
+
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown',
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp',
+            },
+            preConfirm: async () => {
+                // Lógica antes de la confirmación
+            },
+            htmlMode: true
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                /* handlePayment(); */
+
+                Swal.fire({
+                    title: "Redirigiendo a Mercado Pago",
+                    text: "Aguarde unos segundos",
+                    /* icon: "success", */
+                    allowOutsideClick: false,
+                    didOpen:()=>Swal.showLoading()
+                }).then(() => {
+
+                
+                
+                // window.history.back();
+
+                });
+            }
+            else {
+                // Restablecer valores al cancelar
+                /* const parsedData = JSON.parse(storedInput);
+                setInput({
+                    origin: parsedData.origin,
+                    destination: parsedData.destination,
+                    date: parsedData.date,
+                    hour: parsedData.hour,
+                    quantityPassengers: parsedData.quantityPassengers
+                }); */
+            }
+        });
+    
+        }
+  }, [infoConfirmacionViaje]);
 
 
   //*************RUTAS*************** */
@@ -78,19 +148,37 @@ const SolicitudwViajeForm = () => {
   //********************************** */
 
   const handleChange = (e) => {
-    
+    /* setCantidadPasajerosSeleccionada(e.target.value) */
     setInput({
+        ...input,
         origin: origenSeleccionado,
         destination: destinoSeleccionado,
-        date: parsedData.date,
-        hour: parsedData.hour,
-        quantityPassengers: parsedData.quantityPassengers
+        userId: currentUserId,
+        /* quantityPassengers: cantidadPasajerosSeleccionada */
     });
-  
+
+    console.log(input)
     
   };
 
+  const handleSubmit =(event) => {
 
+    setInput({
+        ...input,
+        /* quantityPassengers: cantidadPasajerosSeleccionada */
+    });
+
+    console.log(input)
+    localStorage.setItem('solicitudViajeInput', JSON.stringify(input));
+
+    dispatch(postNewViaje(input));
+    };
+
+
+    /* console.log((JSON.parse(localStorage.getItem('solicitudViajeInput'))).destination) */
+    const [horaSeleccionada, setHoraSeleccionada] = useState(0);
+    const [fechaSeleccionada, setFechaSeleccionada] = useState(0);
+    const [cantidadPasajerosSeleccionada, setCantidadPasajerosSeleccionada] = useState(0);
 
   return (
     <Box marginTop='10rem'>
@@ -100,13 +188,20 @@ const SolicitudwViajeForm = () => {
         </Heading>
 
     <Formik  
-      initialValues={{ name: 'hola' }}
-      onSubmit={(values, actions) => {
+      initialValues={{
+        /* origin: (JSON.parse(localStorage.getItem('solicitudViajeInput'))).origin || '', */
+        /* destination: (JSON.parse(localStorage.getItem('solicitudViajeInput'))).destination || '', */
+        /* date: parsedInput.date || '',
+        hour: parsedInput.hour || '',
+        quantityPassengers: parsedInput.quantityPassengers || '', */
+      }}
+      onSubmit={handleSubmit}
+      /* {(values, actions) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2))
           actions.setSubmitting(false)
         }, 1000)
-      }}
+      }} */
     >
       {(props) => (
         <Form>
@@ -123,6 +218,7 @@ const SolicitudwViajeForm = () => {
                 placeholder="Selecciona un origen"
                 value={origenSeleccionado}
                 onChange={(e) => setOrigenSeleccionado(e.target.value)}/* Setea directamente para determinar los detinos */
+                name="origin"
                 >
                     {origenes.map((origen) => (
                         <option key={origen} value={origen}>
@@ -138,7 +234,9 @@ const SolicitudwViajeForm = () => {
                 >Hasta:</FormLabel>
                 <Select
                 placeholder="Selecciona el destino"
+                value={destinoSeleccionado}
                 onChange={(e) => setDestinoSeleccionado(e.target.value)}
+                name="destino"
                 >
                     {destinosDelOrigen.map((destino) => (
                         <option key={destino} value={destino}>
@@ -158,10 +256,13 @@ const SolicitudwViajeForm = () => {
                     size="md"
                     type="date"
                     name='date'
+                    value={fechaSeleccionada}
                     /* value={input.date}
                     onChange={handleChange}*/
                     onChange={(e) => {
-                        e.target.value===currentDate?setMinHour(currentHour):null //Si fecha actual, hora mínima actual
+                        e.target.value===currentDate?setMinHour(currentHour):null; //Si fecha actual, hora mínima actual
+                        setInput({...input,date:e.target.value});
+                        setFechaSeleccionada(e.target.value);
                       }}
                     min={currentDate}  
                 />
@@ -176,8 +277,13 @@ const SolicitudwViajeForm = () => {
                     type='time'
                     placeholder='Hora'
                     name='hour'
+                    value={horaSeleccionada}
                     /* value={input.hour}
                     onChange={handleChange} */
+                    onChange={(e) => {
+                        setInput({...input,hour:e.target.value});
+                        setHoraSeleccionada(e.target.value);
+                      }}
                     min={minHour} 
                 />
 
@@ -190,9 +296,15 @@ const SolicitudwViajeForm = () => {
                 bg="white"  
                 /* placeholder={parsedData.quantityPassengers} 
                 value ={parsedData.quantityPassengers} */ 
+                value ={cantidadPasajerosSeleccionada}
                 id='pasajeros' 
                 name='quantityPassengers' 
-                /* onChange={handleChange} */ 
+                onChange={(e)=>{
+                    console.log(e.target.value)
+                    setInput({...input,quantityPassengers:e.target.value});
+                    setCantidadPasajerosSeleccionada(e.target.value)
+                    handleChange
+                }} 
                 >
                     {(() => {
                     setMaxPasajeros((origenSeleccionado === "AEROPUERTO TALARA" && destinoSeleccionado === "MANCORA") ||
