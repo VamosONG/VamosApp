7c65045039e293c04fbce323083ee0220403321d
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-01-28 15:29:00 -0300

    Al actualizar muestra barra de carga hasta que comprueba currentUser

--- front/src/components/navBar/NavBar.jsx
+++ front/src/components/navBar/NavBar.jsx
@@ -7,6 +7,7 @@ import {
   Avatar,
   AvatarGroup,
   useMediaQuery,
+  Progress
 } from "@chakra-ui/react";
 import { Link, useLocation } from "react-router-dom";
 import Vamos from "../../assets/logoblanco.png";
@@ -14,6 +15,7 @@ import MobileNavbar from "../navBar/mobileNavbar/mobileNavbar";
 import { useSelector } from "react-redux";
 import SlideEx from "../../views/Forms/ViewForm";
 import LogOut from "../../views/Forms/LogOut/logout";
+import {verificationComplete} from '../../context/authContext';
 
 const NavBar = () => {
   const { currentUser } = useSelector((state) => state);
@@ -33,152 +35,151 @@ const NavBar = () => {
     };
   }, []);
 
-  const [role,setRole]=useState("notUser")
- const handleClick=(role)=>{
-  if (role==='usuario'){setRole('user')}
-  if (role==='admin'){setRole('admin')}
-  
- }
- return (
-  <>
-    {isMobile ? (
-      <MobileNavbar />
-    ) : (
-      <Flex
-        as="nav"
-        bg={
-          location.pathname === "/"
-            ? navBackground
-              ? "#009ED1"
-              : "transparent"
-            : "#009ED1"
-        }
-        alignItems="center"
-        justify="space-between"
-        h="100px"
-        w="100%"
-        position="fixed"
-        top="0"
-        left="0"
-        zIndex="999"
-        px="4"
-        transition="background 0.5s ease"
-      >
-        <Box>
-          <Image src={Vamos} alt="Vamos" w="150px" />
-        </Box>
+ if (!verificationComplete) {
+  return <div><Progress size='lg' isIndeterminate /></div>;
+}else{
 
-        <Box w="100%" alignContent="center" justifyContent="center">
-          <Flex justify="center" alignItems="center">
-            {currentUser.admin && currentUser.admin ? (
-              <Box>
-                <Flex>
-                  <Link to="/">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      INICIO
-                    </Button>
-                  </Link>
-                  
-                  <Link to="/solicitudesDeViajes">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      SOLICITUDES DE VIAJE
-                    </Button>
-                  </Link>
-
-                  {/* <Link to="/detail">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      CONDUCTORES
-                    </Button>
-                  </Link> */}
-
-
-                  <Link to="/profileAdmin">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      MI PERFIL
-                    </Button>
-                  </Link>
-
-                  <Link to='/editPrices'>
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      CAMBIAR PRECIOS DE VIAJES
-                    </Button>
-                  </Link>
-                </Flex>
-              </Box>
-            ) : currentUser.admin === false ? (
-              <Box>
-                <Flex>
-                <Link to="/">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      INICIO
-                    </Button>
-                  </Link>
-
-                  <Link to="/solicitarViaje">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      SOLICITAR VIAJE
-                    </Button>
-                  </Link>
-
-                  <Link to="/profileUser">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      MI PERFIL
-                    </Button>
-                  </Link>
-
-                  <Link to="/questions">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      PREGUNTAS FRECUENTES
-                    </Button>
-                  </Link>
-
-                  <Link to='/review&reseña'>
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      RESEÑA DE TU VIAJE
-                    </Button>
-                  </Link>
-                
-                </Flex>
-              </Box>
-            ) : (
-              <Box>
-                <Flex>
-                  <Link to="/">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      INICIO
-                    </Button>
-                  </Link>
-
-                  <Link to="/about">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      NOSOTROS
-                    </Button>
-                  </Link>
-
-                  <Link to="/questions">
-                    <Button colorScheme="#009ED1" fontSize="1xl">
-                      PREGUNTAS FRECUENTES
-                    </Button>
-                  </Link>
-                  {currentUser && currentUser.admin ? <LogOut /> : null}
-                </Flex>
-              </Box>
-            )}
-          </Flex>
-        </Box>
-
-        <Box>
-          <AvatarGroup spacing="1rem" mx="20px">
-            {currentUser.admin && currentUser.admin ? (
-              <Avatar size="md" name="Ryan Florence" bg="#009ED1" src={currentUser.photoURL} />
-            ) : null}
-            <SlideEx />
-          </AvatarGroup>
-        </Box>
-      </Flex>
-    )}
-  </>
-);
+  return (
+   <>
+     {isMobile ? (
+       <MobileNavbar />
+     ) : (
+       <Flex
+         as="nav"
+         bg={
+           location.pathname === "/"
+             ? navBackground
+               ? "#009ED1"
+               : "transparent"
+             : "#009ED1"
+         }
+         alignItems="center"
+         justify="space-between"
+         h="100px"
+         w="100%"
+         position="fixed"
+         top="0"
+         left="0"
+         zIndex="999"
+         px="4"
+         transition="background 0.5s ease"
+       >
+         <Box>
+           <Image src={Vamos} alt="Vamos" w="150px" />
+         </Box>
+ 
+         <Box w="100%" alignContent="center" justifyContent="center">
+           <Flex justify="center" alignItems="center">
+             {currentUser.admin && currentUser.admin ? (
+               <Box>
+                 <Flex>
+                   <Link to="/">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       INICIO
+                     </Button>
+                   </Link>
+                   
+                   <Link to="/solicitudesDeViajes">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       SOLICITUDES DE VIAJE
+                     </Button>
+                   </Link>
+ 
+                   {/* <Link to="/detail">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       CONDUCTORES
+                     </Button>
+                   </Link> */}
+ 
+ 
+                   <Link to="/profileAdmin">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       MI PERFIL
+                     </Button>
+                   </Link>
+ 
+                   <Link to='/editPrices'>
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       CAMBIAR PRECIOS DE VIAJES
+                     </Button>
+                   </Link>
+                 </Flex>
+               </Box>
+             ) : currentUser.admin === false ? (
+               <Box>
+                 <Flex>
+                 <Link to="/">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       INICIO
+                     </Button>
+                   </Link>
+ 
+                   <Link to="/solicitarViaje">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       SOLICITAR VIAJE
+                     </Button>
+                   </Link>
+ 
+                   <Link to="/profileUser">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       MI PERFIL
+                     </Button>
+                   </Link>
+ 
+                   <Link to="/questions">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       PREGUNTAS FRECUENTES
+                     </Button>
+                   </Link>
+ 
+                   <Link to='/review&reseña'>
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       RESEÑA DE TU VIAJE
+                     </Button>
+                   </Link>
+                 
+                 </Flex>
+               </Box>
+             ) : (
+               <Box>
+                 <Flex>
+                   <Link to="/">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       INICIO
+                     </Button>
+                   </Link>
+ 
+                   <Link to="/about">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       NOSOTROS
+                     </Button>
+                   </Link>
+ 
+                   <Link to="/questions">
+                     <Button colorScheme="#009ED1" fontSize="1xl">
+                       PREGUNTAS FRECUENTES
+                     </Button>
+                   </Link>
+                   {currentUser && currentUser.admin ? <LogOut /> : null}
+                 </Flex>
+               </Box>
+             )}
+           </Flex>
+         </Box>
+ 
+         <Box>
+           <AvatarGroup spacing="1rem" mx="20px">
+             {currentUser.admin && currentUser.admin ? (
+               <Avatar size="md" name="Ryan Florence" bg="#009ED1" src={currentUser.photoURL} />
+             ) : null}
+             <SlideEx />
+           </AvatarGroup>
+         </Box>
+       </Flex>
+     )}
+   </>
+ );
+}
 };
 
 export default NavBar;
--- front/src/context/authContext.jsx
+++ front/src/context/authContext.jsx
@@ -13,7 +13,6 @@ import { useNavigate } from "react-router";
 import { getUserByEmail } from "../redux/actions";
 import { useDispatch } from "react-redux";
 
-
 // Creo un contexto para pasar datos a todos los componentes sin tener que pasar props manualmente
 export const authContext = createContext();
 
@@ -28,6 +27,8 @@ export const useAuth = () => {
 
 }
 
+let verificationComplete= false;
+
 // este es el proveedor del contexto de autenticacion. Envuelve en app a todos los componentes para que puedan acceder a las funciones 
 // de autenticacion a traves del contexto
 export function AuthProvider({ children }) {
@@ -35,6 +36,8 @@ export function AuthProvider({ children }) {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const [user, setUser] = useState("")
+    //const [verificationComplete, setVerificationComplete] = useState(false);  // Estado para controlar la finalización de la verificación
+
     useEffect(() => {
         const suscribed = onAuthStateChanged(auth, (currentUser) => {
             if (!currentUser) {
@@ -46,11 +49,11 @@ export function AuthProvider({ children }) {
                 console.log(currentUser);
                 currentUser
                 dispatch(getUserByEmail(currentUser.email))
-
             }
+            verificationComplete=true;
         })
         return () => suscribed()
-    }, [])
+    }, [/* auth, dispatch */])
 
     // const render = () => {
     //     if (!user) {
@@ -116,3 +119,6 @@ export function AuthProvider({ children }) {
         {children}
     </authContext.Provider>)
 }
+
+export {verificationComplete};
+
