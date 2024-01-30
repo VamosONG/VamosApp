b4135686a37658462b2a0697438fa55b18a39f9c
unknown <rocio.sosa94@gmail.com>
2024-01-30 02:02:50 -0300

    no se recarga la pagina

--- front/src/utils/ProtectedRoute.jsx
+++ front/src/utils/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet} from "react-router-dom";
 const ProtectedRoutes = ({isAllowed, children}) => {
 
     const {currentUser} = useSelector(state=>state)
-    if(!currentUser.name){
+    if(!currentUser.id){
       return  <Navigate to="/" />
     
     }
